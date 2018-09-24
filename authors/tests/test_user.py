a9ed5e16fd153f35645264c1c8945efe5ffa5a97
Kenneth Ddumba <kennethddumba@Kenneths-MacBook-Pro.local>
2018-09-24 20:44:43 +0300

    Modified tests

--- authors/tests/test_user.py
+++ authors/tests/test_user.py
@@ -2,21 +2,22 @@ from authors.apps.authentication.models import User
 from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APIClient
+import json
 class TestUser(TestCase):
 
     def setUp(self):
         """test class set up method"""
         self.client=APIClient()
         self.reg_data= {"user": {
-            "username":"kenneth051",
-            "email":"ken@ken.com",
-            "password":"12345678"}
+            "username":"test",
+            "email":"test@test.com",
+            "password":"test_password"}
         }
+        self.user_data={'email':'test@test.com','username':'test'}
 
     def test_creating_user(self):
         """"This method tests creating a new user"""
-        data={'email':'ken@ken.com','username':'kenneth051'}
         response = self.client.post("/api/users/", self.reg_data, format="json")
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertIn(data['email'],response.data['email'])
-        self.assertIn(data['username'],response.data['username'])
+        self.assertIn(self.user_data['email'],response.data['email'])
+        self.assertIn(self.user_data['username'],response.data['username'])
