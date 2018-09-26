0918bbda10e8cdef906b9fda88e70b2d00def515
Edward Kigozi <edwardkigozi@Edwards-MacBook-Pro.local>
2018-09-26 20:11:47 +0300

    Setup article views

--- authors/apps/articles/views.py
+++ authors/apps/articles/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.generics import RetrieveUpdateAPIView
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .renderers import ArticleJSONRenderer
+from .serializers import ArticleSerializer
+
+class ArticleCreationAPIView(APIView):
+    permission_classes = (AllowAny,)
+    renderer_classes = (ArticleJSONRenderer,)
+    serializer_class = ArticleSerializer
+    def post(self, request):
+        article = request.data.get('article')
+        serializer = self.serializer_class(data=article)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
