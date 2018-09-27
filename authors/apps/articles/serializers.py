f5409dc5cd9d84371df513b23e98ce68053a7c31
Edward Kigozi <edwardkigozi@Edwards-MacBook-Pro.local>
2018-09-27 15:17:13 +0300

    Remove redundatant code from article serializer

--- authors/apps/articles/serializers.py
+++ authors/apps/articles/serializers.py
@@ -6,22 +6,16 @@ from rest_framework import serializers
 from .models import Article
 
 class ArticleSerializer(serializers.ModelSerializer):
-    """ Handles the serialization and deserialization of Article objects"""
-    id = serializers.IntegerField(read_only=True)
-    title=serializers.CharField(required=True, max_length=255)
-    #slug = serializers.SlugField()
-    description = serializers.CharField(max_length=500)
-    body = serializers.CharField()
-    
+    """ 
+    Handles the serialization and deserialization of Article objects
+    """
 
     def create(self,validated_data):
         """
         Create and return a new article instance given the validated data 
         """
         return Article.objects.create(**validated_data)
-
-
-    
+ 
     class Meta:
         model = Article
-        fields = ("id","title","description","body","tagList","author")
+        fields = ("title", "description", "body", "tagList", "author")
