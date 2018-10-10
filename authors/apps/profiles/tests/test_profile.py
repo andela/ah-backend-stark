ee77846083ea56b2e826fe284f0785736d865800
kenneth051 <kjdumba@gmail.com>
2018-10-10 11:37:13 +0300

    ft:list all users profiles [Delivers #160584007]
    Modify code
    
    Increase coverage
    
    Modify code
    
    Modify code
    
    Add more tests
    
    Modify profile tests method doc strings

--- authors/apps/profiles/tests/test_profile.py
+++ authors/apps/profiles/tests/test_profile.py
@@ -9,30 +9,48 @@ class ProfileUser(BaseTest):
     """This class tests activities concerning user model"""
 
     def test_fetching_profile(self):
-        """"This method tests creating a new user"""
-        response = self.client.get("/api/profiles/test123/",
+        """"This method tests fetching a user's profile"""
+        response = self.client.get("/api/profile/test123/",
                                    format="json")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         assert "username" in response.data
+        assert "bio" in response.data
+        assert "location" in response.data
 
     def test_fetching_missing_profile(self):
-        """"This method tests creating a new user"""
-        response = self.client.get("/api/profiles/testuuuu/",
+        """"This method tests fetching a profile that doesn't exists"""
+        response = self.client.get("/api/profile/testuuuu/",
                                    format="json")
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
         assert "errors" in response.data
 
-    def test_editing_profile(self):
-        """"This method tests updating in a user profile"""
-        response = self.client.put("/api/profile/update/",
+    def test_editing_different_profile(self):
+        """"This method tests updating a different user' profile"""
+        response = self.client.put("/api/profile/test1234/",
+                                   self.profile_update,
+                                   format="json")
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        assert "error" in response.data
+
+    def test_editing_your_profile(self):
+        """"This method tests updating a user profile"""
+        response = self.client.put("/api/profile/test123/",
                                    self.profile_update,
                                    format="json")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIn(self.profile_update['profile']['username'], response.data['username'])
         self.assertIn(self.profile_update['profile']['bio'], response.data['bio'])
 
+    def test_editing_with_another_users_name(self):
+        """"This method tests updating a profile changing username to another user's username"""
+        response = self.client.put("/api/profile/test123/",
+                                   self.profile_update1,
+                                   format="json")
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        assert "error" in response.data  
+
     def test_invalid_token(self):
-        """"This method tests updating in a user profile"""
+        """"This method tests updating a profile with an invalid token"""
         client = APIClient()
         client.credentials(HTTP_TOKEN="uuyytttt")
         response = client.put("/api/profile/update/",
@@ -46,3 +64,10 @@ class ProfileUser(BaseTest):
         self.assertEqual(str(entry.bio), "i love me")
         self.assertEqual(str(entry.location), "mbuya")
         self.assertEqual(str(entry.fun_fact), "food")
+
+    def test_fetching_all_profiles(self):
+        """"This method tests getting all profiles"""
+        response = self.client.get("/api/profiles/",
+                                   format="json")                          
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        assert "Authors" in response.data   
--- authors/apps/profiles/views.py
+++ authors/apps/profiles/views.py
@@ -1,6 +1,6 @@
 """views for profile app"""
 from rest_framework import status
-from rest_framework.generics import RetrieveUpdateAPIView
+from rest_framework.generics import RetrieveUpdateAPIView,RetrieveAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from .models import Profile
@@ -19,7 +19,6 @@ class UserProfile(RetrieveUpdateAPIView):
 
         """ function to retrieve user profile information """
         try:
-
             profile = Profile.objects.select_related('user').get(
                 user__username=username)
 
@@ -32,27 +31,47 @@ class UserProfile(RetrieveUpdateAPIView):
 
     def update(self, request, *args, **kwargs):
         """User profile update information """
+        param_name = self.kwargs["username"]
+        active_user = request.user.username
+        if param_name == active_user:
+            user_data = request.data.get('profile', {})
 
-        user_data = request.data.get('profile', {})
+            serializer_data = {
+                'username': user_data.get('username', request.user.username),
+                'bio': user_data.get('bio', request.user.profile.bio),
+                'location': user_data.get('location', request.user.profile.location),
+                'fun_fact': user_data.get('fun_fact', request.user.profile.fun_fact),
+                'image': user_data.get('image', request.user.profile.image)
+            }
 
-        serializer_data = {
-            'username': user_data.get('username', request.user.username),
-            'bio': user_data.get('bio', request.user.profile.bio),
-            'location': user_data.get('location', request.user.profile.location),
-            'fun_fact': user_data.get('fun_fact', request.user.profile.fun_fact),
-            'image': user_data.get('image', request.user.profile.image)
-        }
+            serializer = self.serializer_class(
+                request.user.profile, data=serializer_data, context={'request': request}, partial=True
+            )
+            serializer.is_valid(raise_exception=True)
+            serializer.update(request.user.profile, serializer_data)
 
-        serializer = self.serializer_class(
-            request.user.profile, data=serializer_data, context={'request': request}, partial=True
-        )
-        serializer.is_valid(raise_exception=True)
-        serializer.update(request.user.profile, serializer_data)
+            try:
+                serializer.update(request.user, serializer_data)
+            except:
+                return Response({"error": "Username or email already exist, create a unique one"},
+                                status.HTTP_400_BAD_REQUEST)
 
-        try:
-            serializer.update(request.user, serializer_data)
-        except:
-            return Response({"error": "Username or email already exist, create a unique one"},
-                            status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response({"error": "You can only update your own profile"},
+                                status.HTTP_400_BAD_REQUEST)
 
-        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class ListProfiles(RetrieveAPIView):
+    permission_classes = (IsAuthenticated,)
+    renderer_classes = (ProfileJSONRenderer,)
+    serializer_class = ProfileSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+
+        """ function to retrieve user profile information """
+
+        users=Profile.objects.all()
+        serializer = self.serializer_class(users,many=True)
+        return Response({"Authors":serializer.data}, status=status.HTTP_200_OK)
+
