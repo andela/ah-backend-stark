753ed2b1458e8093161a90b1f2d70349ca79ea44
kenneth051 <kjdumba@gmail.com>
2018-10-10 11:37:13 +0300

    ft:list all users profiles [Delivers #160584007]
    Modify code
    
    Increase coverage
    
    Modify code
    
    Modify code
    
    Add more tests
    
    Modify profile tests method doc strings

--- authors/apps/profiles/tests/__init__.py
+++ authors/apps/profiles/tests/__init__.py
@@ -14,12 +14,26 @@ class BaseTest(TestCase):
             "email":"test1123@test.com",
             "password":"testpassword"}
                          }
+
+        self.reg_data2 = {"user": {
+            "username":"test1234",
+            "email":"test11234@test.com",
+            "password":"testpassword"}
+                         } 
+                                                  
         self.profile_update = {"profile": {
             "username":"test1",
             "email":"test6@test.com",
             "bio":"test",
             "location":"mbuya"}
                                }
+        self.profile_update1 = {"profile": {
+            "username":"test1234",
+            "email":"test6@test.com",
+            "bio":"test",
+            "location":"mbuya"}
+                               }                       
+        self.register_user = self.client.post("/api/users/", self.reg_data2, format="json")                       
         self.register_user = self.client.post("/api/users/", self.reg_data, format="json")
         token1 = self.register_user.data["token"]
         self.client.credentials(HTTP_TOKEN=token1)
