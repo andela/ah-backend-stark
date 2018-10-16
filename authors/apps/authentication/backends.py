73a1274f143f2922ef78d562d5a56d2da19c486a
Nakimera Prossy <nakimeraprossy@Nakimeras-MacBook-Pro.local>
2018-10-16 13:07:59 +0300

    configure pep8 for travis builds

--- authors/apps/authentication/backends.py
+++ authors/apps/authentication/backends.py
@@ -23,21 +23,8 @@ class JWTAuthentication(authentication.BaseAuthentication):
         Try to authenticate the given credentials. If authentication is
         successful, return the user and token. If not, throw an error.
         """
-        try:
-            payload = jwt.decode(token, settings.SECRET_KEY)
-        except:
-            msg = 'The token is invalid'
-            raise exceptions.AuthenticationFailed(msg)
-
-        try:
-            user = User.objects.get(pk=payload['id'])
-        except User.DoesNotExist:
-            msg = 'This token does not belong to any user'
-            raise exceptions.AuthenticationFailed(msg)
-
-        if not user.is_active:
-            msg = 'This user has been deactivated.'
-            raise exceptions.AuthenticationFailed(msg)
+        payload = self.decode_token(token)
+        user = self.get_user(payload)
 
         if not user.is_verified:
             msg = ('This user account is not verified. \
@@ -51,20 +38,30 @@ class JWTAuthentication(authentication.BaseAuthentication):
             Try to authenticate the given credentials. If authentication is
             successful, return the user and token. If not, throw an error.
             """
-            try:
-                payload = jwt.decode(token, settings.SECRET_KEY)
-            except:
-                msg = 'The token is invalid'
-                raise exceptions.AuthenticationFailed(msg)
 
-            try:
-                user = User.objects.get(pk=payload['id'])
-            except User.DoesNotExist:
-                msg = 'This token does not belong to any user'
-                raise exceptions.AuthenticationFailed(msg)
-
-            if not user.is_active:
-                msg = 'This user has been deactivated.'
-                raise exceptions.AuthenticationFailed(msg)
+            payload = self.decode_token(token)
+            user = self.get_user(payload)
 
             return (user, token)
+
+    def decode_token(self, token):
+        try:
+            payload = jwt.decode(token, settings.SECRET_KEY)
+        except Exception as e:
+            msg = 'The token is invalid'
+            raise exceptions.AuthenticationFailed(msg)
+
+        return payload
+
+    def get_user(self, payload):
+        try:
+            user = User.objects.get(pk=payload['id'])
+        except User.DoesNotExist:
+            msg = 'This token does not belong to any user'
+            raise exceptions.AuthenticationFailed(msg)
+
+        if not user.is_active:
+            msg = 'This user has been deactivated.'
+            raise exceptions.AuthenticationFailed(msg)
+
+        return user
